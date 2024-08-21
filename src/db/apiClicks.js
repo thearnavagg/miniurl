62af52bf3018eddd9533ce600357fc58b1805fbb
Arnav Aggarwal <arnavaggarwal2012@gmail.com>
2024-08-21 15:05:02 +0530

    solved errors

--- src/db/apiClicks.js
+++ src/db/apiClicks.js
@@ -12,30 +12,32 @@ export async function getClicksForAllUrls(urlIds) {
     return data;
 }
 
+const parser = new UAParser();
+
 export const storeClicks = async ({ id, original_url }) => {
-    const parser = new UAParser();
     try {
+
         const res = parser.getResult();
-        const device = res.device.type || 'unknown';
+        const device = res.device.type || "desktop";
 
         const response = await fetch("https://ipapi.co/json");
-        const { city, country_name } = await response.json();
+        const { city, country_name: country } = await response.json();
 
         await supabase.from("clicks").insert({
             url_id: id,
             city: city,
-            country: country_name,
+            country: country,
             device: device,
         });
 
         window.location.href = original_url;
     } catch (error) {
-        console.error("Error recording clicks: ", error);
+        console.error("Error recording click:", error);
     }
-}
+};
 
 export async function aboutClicks(url_id) {
-    const { data, error } = await supabase.from("clicks").select("*").eq("url_id", url_id)
+    const { data, error } = await supabase.from("clicks").select("*").eq("url_id", url_id);
 
     if (error) {
         console.log(error.message);
